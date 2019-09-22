2231ea7a4399f1803f9dce104e662455e87a3b96
JyunpeiKuroda <totoro.black6996@gmail.com>
2019-09-22 19:21:41 +0900

    edit-controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,18 @@
 class ItemsController < ApplicationController
+
+def index
+end
+
+def new
+end
+
+def show
+end
+
+def edit
+end
+
+def delete
+end
+
 end
--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,2 +1,18 @@
 class MypagesController < ApplicationController
+
+def index
+end
+
+def new
+end
+
+def show
+end
+
+def edit
+end
+
+def delete
+end
+
 end
