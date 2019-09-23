3bf8cf734a5ddf55be3674b6e2248b65f3aa4117
Kosuke-I <ki.program0915@gmail.com>
2019-09-23 16:39:21 +0900

    #11 create show.html.haml
    confirm_purchase_items.html.hamlを作成したかったが、ルーティングなかったため、show.html.hamlに作成した

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,18 @@
 class ItemsController < ApplicationController
 
-def index
-end
+  def index
+  end
 
-def new
-end
+  def new
+  end
 
-def show
-end
+  def show
+  end
 
-def edit
-end
+  def edit
+  end
 
-def destroy
-end
+  def destroy
+  end
 
 end
