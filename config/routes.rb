764d704aa453c2b7fb59eb4b0b8f700ee7c02085
ryo-ima <i.riino5y@gmail.com>
2019-09-24 14:03:53 +0900

    add_devise_scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/users/signup/registration', to: 'users/registrations#new'
+    get '/users/signup', to: 'users/registrations#registration'
+  end
   root "dtests#index"
   resources :items, only: [:index, :new, :show, :edit, :destroy]
   resources :mypages, only: [:index, :new, :show, :edit, :destroy]
