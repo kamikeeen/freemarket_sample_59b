a393fc6ebf7da67292da8e6c444f7816ba095a4e
ryo-ima <i.riino5y@gmail.com>
2019-09-24 20:59:33 +0900

    #7 create_user_sms_confirm_view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
-    get '/users/signup/registration', to: 'users/registrations#new'
     get '/users/signup', to: 'users/registrations#registration'
+    get '/users/signup/registration', to: 'users/registrations#new'
+    get '/users/signup/sms_confirmation/', to:'users/registrations#sms_confirmation'
+    get '/users/signup/sms/', to:'users/registrations#sms'
   end
   root "dtests#index"
   resources :items, only: [:index, :new, :show, :edit, :destroy]
