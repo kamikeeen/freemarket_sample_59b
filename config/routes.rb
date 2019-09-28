14c8aa77fef7311c073001c3fc1dca46d57795db
ryo-ima <i.riino5y@gmail.com>
2019-09-28 14:41:07 +0900

    #21 edit_routes
    deviseコントローラのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   root "tests#index"
   
-  devise_for :users
+  devise_for :users,
+              controllers: { omniauth_callbacks: 'users/omniauth_callbacks',
+              registrations: 'users/registrations' }
   resources :tests
   resources :signups, only: [:new] do
     collection do
