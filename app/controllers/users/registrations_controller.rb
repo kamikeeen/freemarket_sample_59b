2fb82efee62f303c65ae13791bf6f43f9d1d3426
ryo-ima <i.riino5y@gmail.com>
2019-09-28 14:42:12 +0900

    #21 edit_registrations_controller
    deviseのregistrations_contorollerに条件分岐の記述

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,19 +3,25 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-
-  def registration 
-    
-  end
   # GET /resource/sign_up
   # def new
   #   super
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    #binding.pry
+    if params[:user][:password] == "" #sns登録なら
+      params[:user][:password] = "Devise.friendly_token.first(6)" #deviseのパスワード自動生成機能を使用
+      params[:user][:password_confirmation] = "Devise.friendly_token.first(6)"
+      super
+      # binding.pry
+      sns = SnsCredential.update(user_id:  @user.id)
+    else #email登録なら
+      # binding.pry
+      super
+    end
+  end
 
   # GET /resource/edit
   # def edit
