5b4887845e8cd2e89a427aef9626a71e70c93122
ryo-ima <i.riino5y@gmail.com>
2019-09-28 13:29:51 +0900

    #21 create_model_sns_credential
    sns_credentialテーブルを作成、マイグレーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_22_101250) do
+ActiveRecord::Schema.define(version: 2019_09_28_032853) do
+
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "uid"
+    t.string "provider"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tests", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image"
