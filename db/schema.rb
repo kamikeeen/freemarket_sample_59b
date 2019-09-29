b6d6b68e6e7e3215aa3e37a4db9ae16a95a7fa82
kamikeeen <unjo.kamiken@gmail.com>
2019-09-29 15:06:06 +0900

    #27 add_column-categories_table
    カテゴリーテーブルにsize_idカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_29_055646) do
+ActiveRecord::Schema.define(version: 2019_09_29_060333) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "ancestry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "size_id"
     t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
