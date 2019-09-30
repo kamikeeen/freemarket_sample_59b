4ad30b75d36d218878bc7458f385bce3e81c5dd6
kamikeeen <unjo.kamiken@gmail.com>
2019-09-30 19:05:12 +0900

    #29 change-model-arrival_to_enum
    arrivalをitemのenumで管理

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,6 @@ class Item < ApplicationRecord
   has_many :images
 
   enum status: [:excerent, :great, :good, :normal, :bad, :worse]
-  enum postage: [:exhibitor, :purchaser]
+  enum postage: [:exhibitor_side, :purchaser_side]
+  enum arrival: [:soon, :few_days, :about_week]
 end
