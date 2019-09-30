0e1c16573fbf509d41dce7dd1c718ef7eeb69d5e
kamikeeen <unjo.kamiken@gmail.com>
2019-09-30 19:57:32 +0900

    #29 create-enum_delivery_method
    itemモデルにdelivery_methodをenumで追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,9 @@ class Item < ApplicationRecord
   enum status: [:excerent, :great, :good, :normal, :bad, :worse]
   enum postage: [:exhibitor_side, :purchaser_side]
   enum arrival: [:soon, :few_days, :about_week]
+  enum delivery_method: [:undecide, :rakurakumercari, :yu_mail, :leter_pack, :nurmal_post, :kuroneko, :yu_pack, :click_post, :yu_packet]
+
+  def self.limit_delivery_methods
+    delivery_methods_i18n.slice(:undecide, :yu_mail, :kuroneko, :yu_pack).invert
+  end
 end
