4336f9aee498a6d437f7d08e291446009533d97c
Kosuke-I <ki.program0915@gmail.com>
2019-10-02 23:04:41 +0900

    #28 refactering

--- app/assets/javascripts/items/index.js
+++ app/assets/javascripts/items/index.js
@@ -1,10 +1,7 @@
 $(function() {
   $('.show-item').on('mouseenter', function() {
-    // console.log("ポインターがのりました");
     $(this).css('outline', '1px solid #0099e8');
-    // $(this).animate('outline', '1px solid #0099e8');
   }).on('mouseleave', function() {
-    // console.log("ポインターが外れました");
     $(this).css('outline', '');
   });
 })
