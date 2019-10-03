86f5dd4b9e59fb727a8cdcd1a22bd1037cf4f58e
hideyuckey <54564032+hideyuckey@users.noreply.github.com>
2019-10-03 18:20:48 +0900

    #30-add-validation
    address、user各modelにバリデーションを追加、現状はpresence trueのみ

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,15 @@
 class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
-  belongs_to :user
+  belongs_to :user, optional: true
+
+  validates :familyname,              presence: true
+  validates :firstname,               presence: true
+  validates :familyname_kana,         presence: true
+  validates :firstname_kana,          presence: true
+  validates :zip_code,                presence: true
+  validates :prefecture_id,           presence: true
+  validates :city,                    presence: true
+  validates :address_line,            presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,17 @@ class User < ApplicationRecord
   belongs_to_active_hash :prefecture
   has_many :addresses
   accepts_nested_attributes_for :addresses
+
+  VALID_EMAIL_REGEX =                 /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :nickname,                presence: true, length: {maximum: 20}
+  validates :email,                   presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :password,                presence: true, length: {minimum: 6, maximum: 128}
+  validates :password_confirmation,   presence: true, length: {minimum: 6, maximum: 128}
+  validates :familyname,              presence: true
+  validates :firstname,               presence: true
+  validates :familyname_kana,         presence: true
+  validates :firstname_kana,          presence: true
+  validates :phone_number,            presence: true
+
 end
