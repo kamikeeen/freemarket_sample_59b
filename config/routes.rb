56466e106846ff718ab22a1d0f8862d64d7adc9c
ryo-ima <i.riino5y@gmail.com>
2019-10-03 14:41:00 +0900

    #21 create_SnsCredential_record_sessions
    ログイン画面でSnsCredentialインスタンスをはじめに作成（エラーが出ないように）

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   
   devise_for :users,
               controllers: { omniauth_callbacks: 'users/omniauth_callbacks',
-              registrations: 'users/registrations' }
+              registrations: 'users/registrations',
+              sessions: 'users/sessions' }
   resources :tests
   resources :signups, only: [:index,:new] do
     collection do
