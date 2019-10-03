5cf3f08243569ff4e67825665a20ddb694d96b03
ryo-ima <i.riino5y@gmail.com>
2019-10-03 14:44:35 +0900

    #21 config_validation_and_error_messages
    SnsCredentialsテーブルのバリデーション作成と、エラーメッセージの表示

--- app/models/sns_credential.rb
+++ app/models/sns_credential.rb
@@ -1,3 +1,8 @@
 class SnsCredential < ApplicationRecord
   belongs_to :user, optional: true
+  with_options on: :back_to_login_page do |back_to_login_page|
+    back_to_login_page.validates :uid,                presence: true, uniqueness: true
+    back_to_login_page.validates :provider,           presence: true, uniqueness: true
+    back_to_login_page.validates :user_id,            presence: true
+  end
 end
