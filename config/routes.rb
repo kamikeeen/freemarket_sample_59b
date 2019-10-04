febd67351ebe9633409ae7a7adffdc3c76c2184c
kamikeeen <unjo.kamiken@gmail.com>
2019-10-04 11:59:06 +0900

    edit-routing

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
 
   resources :mypages, only: [:show, :edit] do
     member do
+      get "cards", to:"mypages#cards"
       get "edit_identification", to: "mypages#identification"
       get "logout", to: "mypages#logout"
     end
