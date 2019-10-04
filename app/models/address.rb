6d958e32fd73d671adbf5b5ee832e7850887ff8c
hideyuckey <54564032+hideyuckey@users.noreply.github.com>
2019-10-04 14:15:30 +0900

    #30-add-validation
    Users, Addresses modelにバリデーションを追加

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,10 +3,10 @@ class Address < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to :user, optional: true
 
-  validates :familyname,              presence: true
-  validates :firstname,               presence: true
-  validates :familyname_kana,         presence: true
-  validates :firstname_kana,          presence: true
+  validates :familyname,              presence: true, length: {maximum: 35}
+  validates :firstname,               presence: true, length: {maximum: 35}
+  validates :familyname_kana,         presence: true, length: {maximum: 35}, format: { with: VALID_KANA_REGEX }
+  validates :firstname_kana,          presence: true, length: {maximum: 35}, format: { with: VALID_KANA_REGEX }
   validates :zip_code,                presence: true
   validates :prefecture_id,           presence: true
   validates :city,                    presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,15 +10,17 @@ class User < ApplicationRecord
   accepts_nested_attributes_for :addresses
 
   VALID_EMAIL_REGEX =                 /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_PASSWORD_REGEX =              /\A[a-z0-9]+\z/i
+  VALID_KANA_REGEX =                  /\A[ァ-ヶー－]+\z/
 
   validates :nickname,                presence: true, length: {maximum: 20}
   validates :email,                   presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-  validates :password,                presence: true, length: {minimum: 6, maximum: 128}
-  validates :password_confirmation,   presence: true, length: {minimum: 6, maximum: 128}
-  validates :familyname,              presence: true
-  validates :firstname,               presence: true
-  validates :familyname_kana,         presence: true
-  validates :firstname_kana,          presence: true
-  validates :phone_number,            presence: true
+  validates :password,                presence: true, length: {minimum: 7, maximum: 128}, format: { with: VALID_PASSWORD_REGEX }
+  validates :password_confirmation,   presence: true, length: {minimum: 7, maximum: 128}
+  validates :familyname,              presence: true, length: {maximum: 35}
+  validates :firstname,               presence: true, length: {maximum: 35}
+  validates :familyname_kana,         presence: true, length: {maximum: 35}, format: { with: VALID_KANA_REGEX }
+  validates :firstname_kana,          presence: true, length: {maximum: 35}, format: { with: VALID_KANA_REGEX }
+  validates :phone_number,            presence: true, length: {maximum: 35}
 
 end
