adadcb8eae250ed49e6fe06c6d8f3b9cc1f06fc6
hideyuckey <54564032+hideyuckey@users.noreply.github.com>
2019-10-05 15:25:28 +0900

    #30-add-gem-to-controller-test
    controller test実施のため、gemを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_04_080341) do
+ActiveRecord::Schema.define(version: 2019_10_05_055208) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2019_10_04_080341) do
     t.string "city", null: false
     t.string "address_line", null: false
     t.string "building_name"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
