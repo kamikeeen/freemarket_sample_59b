09f210aa4c4f6693970ea25b523ee9e9b0e5f905
ryo-ima <i.riino5y@gmail.com>
2019-10-05 17:12:11 +0900

    #21 modify_validates
    バリデーションの記述を修正

--- app/models/sns_credential.rb
+++ app/models/sns_credential.rb
@@ -1,8 +1,7 @@
 class SnsCredential < ApplicationRecord
   belongs_to :user, optional: true
-  with_options on: :back_to_login_page do |back_to_login_page|
-    back_to_login_page.validates :uid,                presence: true, uniqueness: true
-    back_to_login_page.validates :provider,           presence: true, uniqueness: true
-    back_to_login_page.validates :user_id,            presence: true
-  end
+  validates :uid,                presence: true, uniqueness: { scope: :provider  }
+  validates :provider,           presence: true
+  validates :user_id,            presence: true
+    
 end
