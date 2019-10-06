a03ad82d8731b99bd65ee892e8e218b485ee1180
ryo-ima <i.riino5y@gmail.com>
2019-10-06 21:13:30 +0900

    #31 Create_edit_js

---
+++ app/assets/javascripts/api/delivery_methods/select_edit.js
@@ -0,0 +1,22 @@
+$(function(){
+  $("#item_postage_side").on("change", function(){
+    if ($(this).val() !== ""){
+      var postage_side = $(this).val();
+
+      $.ajax({
+        url: "/api/delivery_methods/select",
+        type: "get",
+        data: { postage_side: postage_side},
+        dataType: "html"
+      })
+      .done(function(html){
+        $(".items__edit__sell__form__about-delivery-form__delivery_method").remove();
+        $(".items__edit__sell__form__about-delivery-form__post-area").before(html)
+      })
+      .fail(function(){
+      })
+    }else{
+      $(".items__edit__sell__form__about-delivery-form__delivery_method").remove();
+    }
+  })
+})
