3e75497bf743e1f94367d8d82fb731a0b9b13466
ryo-ima <i.riino5y@gmail.com>
2019-10-08 10:33:35 +0900

    #31 edit_set_action_js
    編集時と出品時のjsの切り分け

--- app/assets/javascripts/api/delivery_methods/set_edit.js
+++ app/assets/javascripts/api/delivery_methods/set_edit.js
@@ -1,27 +1,11 @@
 $(function(){
-  $(document).ready(function(){
-    var item_id = $("#item_id").text();
-    $.ajax({
-      url: "/api/delivery_methods/set",
-      type: "get",
-      data: { item_id: item_id },
-      dataType: "html"
-    })
-    .done(function(html){
-      $(".items__edit__sell__form__about-delivery-form__delivery_method").remove();
-      $(".items__edit__sell__form__about-delivery-form__post-area").before(html)
-    })
-    .fail(function(){
-    })
-  });
-  $("#item_postage_side").on("change", function(){
-    if ($(this).val() !== ""){
-      var postage_side = $(this).val();
 
+    var item_id = $("#item_id").text();
+    if (item_id !== ""){
       $.ajax({
-        url: "/api/delivery_methods/select",
+        url: "/api/delivery_methods/set",
         type: "get",
-        data: { postage_side: postage_side},
+        data: { item_id: item_id },
         dataType: "html"
       })
       .done(function(html){
@@ -30,8 +14,25 @@ $(function(){
       })
       .fail(function(){
       })
-    }else{
-      $(".items__edit__sell__form__about-delivery-form__delivery_method").remove();
-    }
-  })
+    $("#item_postage_side").on("change", function(){
+      if ($(this).val() !== ""){
+        var postage_side = $(this).val();
+
+        $.ajax({
+          url: "/api/delivery_methods/select",
+          type: "get",
+          data: { postage_side: postage_side},
+          dataType: "html"
+        })
+        .done(function(html){
+          $(".items__edit__sell__form__about-delivery-form__delivery_method").remove();
+          $(".items__edit__sell__form__about-delivery-form__post-area").before(html)
+        })
+        .fail(function(){
+        })
+      }else{
+        $(".items__edit__sell__form__about-delivery-form__delivery_method").remove();
+      }
+    })
+  }
 })
