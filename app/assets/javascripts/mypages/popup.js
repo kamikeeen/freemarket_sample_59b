34411ce7a8fc6488450b040bccda84a78a0ac22c
hideyuckey <54564032+hideyuckey@users.noreply.github.com>
2019-10-08 15:51:21 +0900

    #34-add-cancel-function-of-popup
    削除確認用opupのキャンセルボタンの実装。

--- app/assets/javascripts/mypages/popup.js
+++ app/assets/javascripts/mypages/popup.js
@@ -1,7 +1,11 @@
 $(function(){
   $(".item-detail__contents__inner__details__confirm-delete-item").on("click", function(e) {
     e.preventDefault();
-    console.log(this)
-    $(".item-delete-confimation-popup").addClass("active")
+    $("#item-delete-confimation-popup").addClass("active");
+  })
+  $(".content__lower").on("click", ".content__lower--cancel" , function(e) {
+    e.preventDefault();
+    console.log(this);
+    $("#item-delete-confimation-popup").removeClass("active");
   })
 })
