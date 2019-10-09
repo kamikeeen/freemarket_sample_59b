777e6a02b5fc3dd1305c111b6694249789067189
ryo-ima <i.riino5y@gmail.com>
2019-10-09 17:31:47 +0900

    #31 delete_comment_out
    コメントアウトを外す

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -3,40 +3,40 @@ require 'rails_helper'
 describe ItemsController do
   
 
-  # describe 'GET #index' do
-  # let!(:images) {create_list(:image, 41)}
-  #   before do
-  #     get :index
-  #   end
+  describe 'GET #index' do
+  let!(:images) {create_list(:image, 41)}
+    before do
+      get :index
+    end
 
-  #   it "assigns @itemsCategory1, @itemsCategory2, @itemsCategory3, @itemsCategory4, @CategoryName1, @CategoryName2, @CategoryName3, @CategoryName4" do
+    it "assigns @itemsCategory1, @itemsCategory2, @itemsCategory3, @itemsCategory4, @CategoryName1, @CategoryName2, @CategoryName3, @CategoryName4" do
 
-  #     category1 = 1
-  #     expect(assigns(:itemsCategory1)).to match(Item.where(category_id: [Category.find(category1).descendant_ids]).sort{|a, b| b.id <=> a.id}.values_at(0..9))
-  #     category = Category.find(category1)
-  #     expect(assigns(:CategoryName1)).to match(category.name)
+      category1 = 1
+      expect(assigns(:itemsCategory1)).to match(Item.where(category_id: [Category.find(category1).descendant_ids]).sort{|a, b| b.id <=> a.id}.values_at(0..9))
+      category = Category.find(category1)
+      expect(assigns(:CategoryName1)).to match(category.name)
 
-  #     category2 = 200
-  #     expect(assigns(:itemsCategory2)).to match(Item.where(category_id: [Category.find(category2).descendant_ids]).sort{|a, b| b.id <=> a.id}.values_at(0..9))
-  #     category = Category.find(category2)
-  #     expect(assigns(:CategoryName2)).to match(category.name)
+      category2 = 200
+      expect(assigns(:itemsCategory2)).to match(Item.where(category_id: [Category.find(category2).descendant_ids]).sort{|a, b| b.id <=> a.id}.values_at(0..9))
+      category = Category.find(category2)
+      expect(assigns(:CategoryName2)).to match(category.name)
 
-  #     category3 = 680
-  #     expect(assigns(:itemsCategory3)).to match(Item.where(category_id: [Category.find(category3).descendant_ids]).sort{|a, b| b.id <=> a.id}.values_at(0..9))
-  #     category = Category.find(category3)
-  #     expect(assigns(:CategoryName3)).to match(category.name)
+      category3 = 680
+      expect(assigns(:itemsCategory3)).to match(Item.where(category_id: [Category.find(category3).descendant_ids]).sort{|a, b| b.id <=> a.id}.values_at(0..9))
+      category = Category.find(category3)
+      expect(assigns(:CategoryName3)).to match(category.name)
 
-  #     category4 = 893
-  #     expect(assigns(:itemsCategory4)).to match(Item.where(category_id: [Category.find(category4).descendant_ids]).sort{|a, b| b.id <=> a.id}.values_at(0..9))
-  #     category = Category.find(category4)
-  #     expect(assigns(:CategoryName4)).to match(category.name)
-  #   end
+      category4 = 893
+      expect(assigns(:itemsCategory4)).to match(Item.where(category_id: [Category.find(category4).descendant_ids]).sort{|a, b| b.id <=> a.id}.values_at(0..9))
+      category = Category.find(category4)
+      expect(assigns(:CategoryName4)).to match(category.name)
+    end
 
-  #   it "renders the :index template" do
-  #     expect(response).to render_template :index
-  #   end
+    it "renders the :index template" do
+      expect(response).to render_template :index
+    end
 
-  # end
+  end
   
   before do
     @another_user = FactoryBot.create(:another_user)
