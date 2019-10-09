09b19dcbf20ba6feaa8777f29dcd6c496ba344d1
hideyuckey <54564032+hideyuckey@users.noreply.github.com>
2019-10-09 15:51:23 +0900

    #34-confirmation-of-items-controller-test
    コンフリクト解消後のテスト確認

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -4,6 +4,6 @@ FactoryBot.define do
     name                  {Faker::Lorem.characters}
     ancestry              {"1"}
     created_at            {Faker::Time.between(from: DateTime.now - 2, to: DateTime.now) }
-    size_id               {1}
+    size
   end
 end
