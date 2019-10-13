63fdf2191e247afa60f6760a9d25c2cc2878581d
Kosuke-I <ki.program0915@gmail.com>
2019-10-13 21:18:28 +0900

    #35 modify show action at categories_controller
    カテゴリーコントローラのshowアクションを修正・追加した。

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,19 +2,22 @@ class CategoriesController < ApplicationController
   # before_action :set_category_list,only:[:show]
 
   def index
-    @parentCategories = Category.where(ancestry: nil);
+    @parentCategories = Category.where(ancestry: nil)
   end
 
   def new
   end
 
   def show
-    @parentCategories = Category.where(ancestry: nil).page(params[:page]).per(5)
+    @parentCategories = Category.where(ancestry: nil)
+
     if Category.find(params[:id]).has_children? then
-      @items = Item.where(category_id: Category.find(params[:id]).descendant_ids).order('id Desc')
+      @items = Item.where(category_id: Category.find(params[:id]).descendant_ids).order('id Desc').page(params[:page]).per(5)
     else
-      @items = Item.where(category_id: params[:id]).order('id Desc')
+      @items = Item.where(category_id: params[:id]).order('id Desc').page(params[:page]).per(5)
     end
+
+    @category = Category.find(params[:id])
     
   end
 
