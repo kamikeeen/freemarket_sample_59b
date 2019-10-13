809deaed0769854469fb3b84581b887f2c43087b
Kosuke-I <ki.program0915@gmail.com>
2019-10-13 12:30:31 +0900

    #35 modify items at show.html.haml
    カテゴリーのshowテンプレートに商品のデータを表示できるようにした

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,7 +9,12 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    @parentCategories = Category.where(ancestry: nil).page(params[:page]).per(5);
+    @parentCategories = Category.where(ancestry: nil).page(params[:page]).per(5)
+    if Category.find(params[:id]).has_children? then
+      @items = Item.where(category_id: Category.find(params[:id]).descendant_ids).order('id Desc')
+    else
+      @items = Item.where(category_id: params[:id]).order('id Desc')
+    end
     
   end
 
