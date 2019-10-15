62273c1a21ad50851db25057fcd23acb78b160ec
kamikeeen <unjo.kamiken@gmail.com>
2019-10-15 14:11:19 +0900

    Update search_controller.rb

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,7 @@ class SearchController < ApplicationController
     if params[:q][:category_id_in] && params[:q][:category_id_in] != "" && Category.find(params[:q][:category_id_in]).has_children?
       params[:q][:category_id_in] = Category.find(params[:q][:category_id_in]).descendants.ids
     end
-    if params[:q][:size_id_in].length == 1 && params[:q][:size_id_in].first != ""
+    if params[:q][:size_id_in] && params[:q][:size_id_in].length == 1 && params[:q][:size_id_in].first != ""
       params[:q][:size_id_in] = Size.find(params[:q][:size_id_in].first).descendants.ids
     end
     @q = Item.ransack(params[:q])
