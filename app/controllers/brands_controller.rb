87d80f1221b20e92513a334aa04b7c8070349c39
ryo-ima <i.riino5y@gmail.com>
2019-10-15 15:30:21 +0900

    #47 create_brand_controller
    ブランドコントローラを作成

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -2,20 +2,11 @@ class BrandsController < ApplicationController
   require 'miyabi'
 
   def index
-    @brandsinitials = Brand.order('name.to_kanhira Desc')
-    binding.pry
+    @brands = Brand.order('name Asc')
   end
 
   def show
-    @parentCategories = Category.where(ancestry: nil)
-
-    if Category.find(params[:id]).has_children? then
-      @items = Item.where(category_id: Category.find(params[:id]).descendant_ids).order('id Desc').page(params[:page]).per(5)
-    else
-      @items = Item.where(category_id: params[:id]).order('id Desc').page(params[:page]).per(5)
-    end
-
-    @category = Category.find(params[:id])
-    
+    @brand = Brand.find(params[:id])
+    @items = Item.where(brand_id: params[:id]).order('id Desc').page(params[:page]).per(5)
   end
 end
