88f45183ae0d0e2dea65f637bbb7393d01c80784
JyunpeiKuroda <totoro.black6996@gmail.com>
2019-10-15 17:09:42 +0900

    test２

---
+++ app/assets/javascripts/items/show.js
@@ -0,0 +1,8 @@
+
+
+// var thumbs =document.querySelectorAll('.picture');
+// for (var i =0 ; i < picture.length; i++ ) {
+//   thumbs[i].onclick = function() {
+//   document.getElementById('picture__m').src = this.dataset.imagesrc;
+//   };
+// }
