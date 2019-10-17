11e3c04f5da4d3abdb640224f3f32a07c3d3ed78
JyunpeiKuroda <totoro.black6996@gmail.com>
2019-10-17 21:33:54 +0900

    re:edit-image
    ・詳細画像のエフェクトの追加
    ・SOLDでも同じエフェクトが行く様に。
    ・カテゴリーのパスの設定

--- app/assets/javascripts/items/show.js
+++ app/assets/javascripts/items/show.js
@@ -1,8 +1,8 @@
-
-
-// var thumbs =document.querySelectorAll('.picture');
-// for (var i =0 ; i < picture.length; i++ ) {
-//   thumbs[i].onclick = function() {
-//   document.getElementById('picture__m').src = this.dataset.imagesrc;
-//   };
-// }
+$(function() {
+  $('.picture__m').eq(0).css("display", "block");
+  $('.picture').hover(function() {
+    $('.picture__m').css("display", "none");
+    var index = $('.picture').index(this);
+    $('.picture__m').eq(index).css("display", "block");
+  });
+});
