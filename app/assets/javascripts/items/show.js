b0a7b3b4e220f792250fbde0d6855acec00720e6
kamikeeen <unjo.kamiken@gmail.com>
2019-10-17 12:12:10 +0900

    Revert "Update show.js"
    This reverts commit 2f06b4bb02312261710077061b2029291ff9b5bb.

--- app/assets/javascripts/items/show.js
+++ app/assets/javascripts/items/show.js
@@ -1,5 +1,6 @@
 $(function() {
   $('.picture__m').eq(0).css("display", "block");
+  //マウスを乗せたら発動
   $('.picture').hover(function() {
     $('.picture__m').css("display", "none");
     var index = $('.picture').index(this);
