463bd89d5fa59a80aa24b184469ea04dcff77ff0
ryo-ima <i.riino5y@gmail.com>
2019-10-17 17:37:02 +0900

    modify_edit_and_purchase
    購入画面と編集画面の修正

--- app/controllers/api/delivery_methods_controller.rb
+++ app/controllers/api/delivery_methods_controller.rb
@@ -2,8 +2,9 @@ class Api::DeliveryMethodsController < ApplicationController
   layout false
 
   def set
-    @delivery_methods = (params[:postage_side] == "exhibitor") ? Item.delivery_methods_i18n : Item.limit_delivery_methods_i18n
-    @delivery_method = Item.find(params[:item_id].to_s).delivery_method
+    item = Item.find(params[:item_id].to_s)
+    @delivery_methods = (item.postage_side == "exhibitor") ? Item.delivery_methods_i18n : Item.limit_delivery_methods_i18n
+    @delivery_method = item.delivery_method
   end
 
   def select
