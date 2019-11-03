16c1416dce0efb780f6b704194d87576514bc353
kamikeeen <unjo.kamiken@gmail.com>
2019-11-03 13:15:27 +0900

    Update search_controller.rb

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,5 +9,7 @@ class SearchController < ApplicationController
     if params[:q][:size_id_in] && params[:q][:size_id_in].length == 1 && params[:q][:size_id_in].first != ""
       params[:q][:size_id_in] = Size.find(params[:q][:size_id_in].first).descendants.ids
     end
+    @q = Item.ransack(params[:q])
+    @items = @q.result(distinct: true).page(params[:page]).per(30)
   end
 end
